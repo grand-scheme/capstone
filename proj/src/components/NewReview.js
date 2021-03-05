d253c647f767fcdbe59036a4e65aed0e81e89f09
Shannon Grantski <grantski@pm.me>
2021-03-05 15:49:38 -0800

    add form for new review

--- proj/src/components/NewReview.js
+++ proj/src/components/NewReview.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import {v4} from 'uuid';
 
 function NewReview(props){
+  function handleNewReview(e){
+    e.preventDefault();
+    console.log(e.target.rating.value);
+  }
+
   return (
     <>
-      ¯\_(ツ)_/¯
+      <form onSubmit={handleNewReview}> {/* NOTE: UNDEFINED HANDLE */}
+        <input 
+          type='text'
+          name='restaurantName'
+          placeholder='Restaurant'
+        />
+        <input 
+          type='text'
+          name='address'
+          placeholder='Address'
+        />
+        <input 
+          type='text'
+          name='location'
+          placeholder='City, State'
+        />
+        <input 
+          type='text'
+          name='rating'
+          placeholder='Rating'
+        />
+        <input 
+          type='text'
+          name='date'
+          placeholder='Date'
+        />
+        <textarea 
+          name='review'
+          placeholder='Write your review here.'
+        />
+        <button type='submit'>Save</button>
+      </form>
     </>
   );
 }
