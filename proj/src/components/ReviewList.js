0e5718ec0b41b8aa49c34c1a94f196a032d7e2cb
Shannon Grantski <grantski@pm.me>
2021-03-05 12:20:49 -0800

    add .map, temporary array

--- proj/src/components/ReviewList.js
+++ proj/src/components/ReviewList.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import Review from './Review';
 
+const tempReviewList = [
+  {
+    restaurantName: "Goldie's",
+    address: '101 1st Ave',
+    location: 'Seattle, WA',
+    rating: '😭',
+    date: '3/5/2021',
+    review: "They ran out of pie..."
+  },
+  {
+    restaurantName: "Bernie's",
+    address: '210 21st Ave',
+    location: 'Seattle, WA',
+    rating: '🙁',
+    date: '3/2/2021',
+    review: "Not great."
+  },
+  {
+    restaurantName: "Clyde's",
+    address: '1011 Cherry St',
+    location: 'Portland, OR',
+    rating: '🥰',
+    date: '2/14/2021',
+    review: "Free dessert"
+  }
+];
+
 function ReviewList(){
   return (
-    <Review 
-      restaurantName = "Goldie's"
-      address = '101 1st Ave'
-      location = 'Seattle, WA'
-      rating = '😭'
-      date = '3/5/2021'
-      review = "They ran out of pie..."
-    />
+    <>
+      {tempReviewList.map((review, index) =>
+        <Review 
+          restaurantName={review.restaurantName}
+          address={review.address}
+          location={review.location}
+          rating={review.rating}
+          date={review.date}
+          review={review.review}
+          key={index}
+        />
+      )}
+    </>
   );
 }
 
