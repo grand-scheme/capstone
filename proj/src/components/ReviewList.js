30a22ebdf7c1291c6634733ac99aa1b14b40158b
Shannon Grantski <grantski@pm.me>
2021-03-05 11:46:36 -0800

    pass down props to review.js

--- proj/src/components/ReviewList.js
+++ proj/src/components/ReviewList.js
@@ -3,7 +3,14 @@ import Review from './Review';
 
 function ReviewList(){
   return (
-    <Review />
+    <Review 
+      restaurantName = "Goldie's"
+      address = '101 1st Ave'
+      location = 'Seattle, WA'
+      rating = '😭'
+      date = '3/5/2021'
+      review = "They ran out of pie..."
+    />
   );
 }
 
