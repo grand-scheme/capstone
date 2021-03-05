13a37134749045a0e079297be688e68cbb5844f8
Shannon Grantski <grantski@pm.me>
2021-03-05 11:46:22 -0800

    set up to pass down props

--- proj/src/components/Review.js
+++ proj/src/components/Review.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-function Review(){
-  const restaurantName = "Goldie's";
-  const address = '101 1st Ave';
-  const location = 'Seattle, WA';
-  const rating = '😭';
-  const date = '3/5/2021';
-  const review = "They ran out of pie...";
+function Review(props){
+  // const restaurantName = "Goldie's";
+  // const address = '101 1st Ave';
+  // const location = 'Seattle, WA';
+  // const rating = '😭';
+  // const date = '3/5/2021';
+  // const review = "They ran out of pie...";
 
   return (
   <>
     <p>
-      {restaurantName} <br />
-      {address} <br />
-      {location} <br />
-      {rating} <br />
-      {date} <br />
-      {review} <br />
+      {props.restaurantName} <br />
+      {props.address} <br />
+      {props.location} <br />
+      {props.rating} <br />
+      {props.date} <br />
+      {props.review} <br />
     </p>
   </>
   )
