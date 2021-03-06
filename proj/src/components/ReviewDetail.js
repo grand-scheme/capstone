a418b011c871b6ee6bd983b93c9506ac5c38a785
Shannon Grantski <grantski@pm.me>
2021-03-06 00:46:57 -0800

    add detail and proptypes

--- proj/src/components/ReviewDetail.js
+++ proj/src/components/ReviewDetail.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ReviewDetail(props){
+  const { review } = props;
+
   return(
     <>
-      ¯\_(ツ)_/¯
+      {review.restaurantName} <br />
+      {review.address} <br />
+      {review.location} <br />
+      {review.rating} <br />
+      {review.date} <br />
+      {review.review} <br />
     </>
   );
 }
 
+ReviewDetail.propTypes ={
+  review: PropTypes.object
+};
 export default ReviewDetail;
