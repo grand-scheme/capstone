363fd43d94ad8c8c3f902f7437fd4761395f0e53
Shannon Grantski <grantski@pm.me>
2021-03-06 00:45:51 -0800

    remove hard data, add click handle

--- proj/src/components/Review.js
+++ proj/src/components/Review.js
@@ -2,23 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Review(props){
-  // const restaurantName = "Goldie's";
-  // const address = '101 1st Ave';
-  // const location = 'Seattle, WA';
-  // const rating = '😭';
-  // const date = '3/5/2021';
-  // const review = "They ran out of pie...";
 
   return (
   <>
-    <p>
-      {props.restaurantName} <br />
-      {props.address} <br />
-      {props.location} <br />
-      {props.rating} <br />
-      {props.date} <br />
-      {props.review} <br />
-    </p>
+    <div onClick = {() => props.whenClicked(props.id)}>
+      {/* NOTE: UNDEFINED WHEN PROP */}
+      <p>
+        {props.restaurantName} <br />
+        {props.address} <br />
+        {props.location} <br />
+        {props.rating} <br />
+        {props.date} <br />
+        {props.review} <br />
+      </p>
+    </div>
   </>
   )
 }
@@ -29,7 +26,9 @@ Review.propTypes = {
   location: PropTypes.string,
   rating: PropTypes.string,
   date: PropTypes.string,
-  review: PropTypes.string
+  review: PropTypes.string,
+  id: PropTypes.string,
+  whenClicked: PropTypes.func
   // add .isRequired to the ones that will be required
 };
 
