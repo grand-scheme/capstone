f8085ad396cfbc845c61e486d95c3c1c45573865
Shannon Grantski <grantski@pm.me>
2021-03-05 16:27:47 -0800

    move and import tempRL

--- proj/src/components/ReviewList.js
+++ proj/src/components/ReviewList.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import Review from './Review';
+import tempReviewList from '../data/temp-review-list'
 
-const tempReviewList = [
-  {
-    restaurantName: "Goldie's",
-    address: '101 1st Ave',
-    location: 'Seattle, WA',
-    rating: '😭',
-    date: '3/5/2021',
-    review: "They ran out of pie..."
-  },
-  {
-    restaurantName: "Bernie's",
-    address: '210 21st Ave',
-    location: 'Seattle, WA',
-    rating: '🙁',
-    date: '3/2/2021',
-    review: "Not great."
-  },
-  {
-    restaurantName: "Clyde's",
-    address: '1011 Cherry St',
-    location: 'Portland, OR',
-    rating: '🥰',
-    date: '2/14/2021',
-    review: "Free dessert"
-  }
-];
-
+let list = tempReviewList
 function ReviewList(){
   return (
     <>
-      {tempReviewList.map((review, index) =>
+      {list.map((review, index) =>
         <Review 
           restaurantName={review.restaurantName}
           address={review.address}
---
+++ proj/src/data/temp-review-list.js
@@ -0,0 +1,28 @@
+const tempReviewList = [
+  {
+    restaurantName: "Goldie's",
+    address: '101 1st Ave',
+    location: 'Seattle, WA',
+    rating: '😭',
+    date: '3/5/2021',
+    review: "They ran out of pie..."
+  },
+  {
+    restaurantName: "Bernie's",
+    address: '210 21st Ave',
+    location: 'Seattle, WA',
+    rating: '🙁',
+    date: '3/2/2021',
+    review: "Not great."
+  },
+  {
+    restaurantName: "Clyde's",
+    address: '1011 Cherry St',
+    location: 'Portland, OR',
+    rating: '🥰',
+    date: '2/14/2021',
+    review: "Free dessert"
+  }
+];
+
+export default tempReviewList;
