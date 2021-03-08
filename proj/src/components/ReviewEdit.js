056e8a0079a16ac1499b8eea713238f519cda191
Shannon Grantski <grantski@pm.me>
2021-03-08 10:21:01 -0800

    add edit

--- proj/src/components/ReviewEdit.js
+++ proj/src/components/ReviewEdit.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import Form from './Form';
+import PropTypes from 'prop-types';
 
-function ReviewEdit(){
+function ReviewEdit(props){
+  const { review } = props;
+  function handleEditConfirmation(e){
+    e.preventDefault();
+    props.onEditReview({
+      id: review.id,
+      restaurantName: e.target.restaurantName.value,
+      address: e.target.address.value,
+      location: e.target.location.value,
+      rating: e.target.rating.value,
+      date: e.target.date.value,
+      review: e.target.review.value
+    });
+    console.log(e.target.rating.value);
+  }
 
+  return (
+    <>
+      <Form 
+        buttonText='Edit Review'
+        formHandler={handleEditConfirmation}
+      />
+    </>
+  );
 }
 
+ReviewEdit.propTypes = {
+  handleEditConfirmation: PropTypes.func,
+  review: PropTypes.object
+}
 export default ReviewEdit;
