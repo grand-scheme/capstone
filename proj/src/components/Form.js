5f48c8da73517d9b612a2467534198aef106d22a
Shannon Grantski <grantski@pm.me>
2021-03-08 12:12:24 -0800

    add linebreaks

--- proj/src/components/Form.js
+++ proj/src/components/Form.js
@@ -5,36 +5,41 @@ function Form(props){
   return(
     <>
       <form onSubmit={props.formHandler}> 
-      {/* NOTE: UNDEFINED HANDLE */}
         <input 
           type='text'
           name='restaurantName'
           placeholder={props.restaurantName}
         />
+        <br />
         <input 
           type='text'
           name='address'
           placeholder={props.address}
         />
+        <br />
         <input 
           type='text'
           name='location'
           placeholder={props.location}
         />
+        <br />
         <input 
           type='text'
           name='rating'
           placeholder={props.rating}
         />
+        <br />
         <input 
           type='text'
           name='date'
           placeholder={props.date}
         />
+        <br />
         <textarea 
           name='review'
           placeholder={props.review}
         />
+        <br />
         <button type='submit'>{props.buttonText}</button>
       </form>
     </>
