8f217f8ab4207eb549a8bfa1e6849d93990cba34
Shannon Grantski <grantski@pm.me>
2021-03-10 23:30:21 -0800

    add further actions

--- proj/src/__tests__/actions/index.test.js
+++ proj/src/__tests__/actions/index.test.js
@@ -8,4 +8,31 @@ describe('review actions', () => {
       id: 1
     });
   });
+  //
+  it('toggleNew should create TOGGLE_NEW action', () => {
+    expect(a.toggleNew()).toEqual({
+      type: c.TOGGLE_NEW
+    });
+  });
+  //
+  it('addReview should create ADD_REVIEW action', () => {
+    expect(a.addReview({
+      restaurantName: "Bernie's",
+      address: '210 21st Ave',
+      location: 'Seattle, WA',
+      rating: '🙁',
+      date: '3/2/2021',
+      review: "Not great.",
+      id:'20'
+    })).toEqual({
+      type: c.ADD_REVIEW,
+      restaurantName: "Bernie's",
+      address: '210 21st Ave',
+      location: 'Seattle, WA',
+      rating: '🙁',
+      date: '3/2/2021',
+      review: "Not great.",
+      id:'20'
+    });
+  });
 });
--- proj/src/actions/index.js
+++ proj/src/actions/index.js
@@ -26,3 +26,7 @@ export const deleteReview = id => ({
   type: c.DELETE_REVIEW,
   id
 });
+
+export const toggleNew = () => ({
+  type: c.TOGGLE_NEW
+});
