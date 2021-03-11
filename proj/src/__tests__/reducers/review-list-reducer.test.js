489c603f7ba7cf2e10450b1a8795b3b05ff16f26
Shannon Grantski <grantski@pm.me>
2021-03-10 21:14:14 -0800

    add review

--- proj/src/__tests__/reducers/review-list-reducer.test.js
+++ proj/src/__tests__/reducers/review-list-reducer.test.js
@@ -1,7 +1,52 @@
 import reviewListReducer from '../../reducers/review-list-reducer';
+import tempReviewList from '../../data/temp-review-list';
+import * as c from '../../actions/ActionTypes';
 
 describe('reviewListReducer', () => {
+  let action;
+  const testData = {
+    restaurantName: "Bernie's",
+    address: '210 21st Ave',
+    location: 'Seattle, WA',
+    rating: '🙁',
+    date: '3/2/2021',
+    review: "Not great.",
+    id:'20'
+  }
   test('Return default state without action passed in', () => {
     expect(reviewListReducer({}, { type: null} )).toEqual({});
+  });
+  // 
+  test('should add review to list', () => {
+    const { 
+      restaurantName,
+      address,
+      location,
+      rating,
+      date,
+      review,
+      id
+    } = testData;
+    action = {
+      type: c.ADD_REVIEW,
+      restaurantName,
+      address,
+      location,
+      rating,
+      date,
+      review,
+      id,
+    };
+    expect(reviewListReducer({}, action)).toEqual({
+      [id] : {
+        restaurantName,
+        address,
+        location,
+        rating,
+        date,
+        review,
+        id,
+      }
+    });
   });
 });
--- proj/src/reducers/review-list-reducer.js
+++ proj/src/reducers/review-list-reducer.js
@@ -1,3 +1,29 @@
+import * as c from '../actions/ActionTypes';
+
 export default (state={}, action) => {
-  return state;
+  const {
+    restaurantName,
+    address,
+    location,
+    rating,
+    date,
+    review,
+    id,
+  } = action;
+  switch (action.type) {
+    case c.ADD_REVIEW:
+      return Object.assign({}, state, {
+        [id]: {
+          restaurantName,
+          address,
+          location,
+          rating,
+          date,
+          review,
+          id,
+        }
+      });
+  default: 
+    return state;
+  }
 };
